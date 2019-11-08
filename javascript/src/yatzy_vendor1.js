2e1a4cd1779fdbf3de955833f36055d352d83d64
Emily Bache <emily.bache@praqma.com>
2019-11-08 10:03:20 +0100

    add dice frequencies function

--- javascript/src/yatzy_vendor1.js
+++ javascript/src/yatzy_vendor1.js
@@ -5,12 +5,21 @@ const YatzyCategoriesEnum = Object.freeze({
     "pair":9, "threeofakind":10, "fourofakind":11,
     "smallstraight":12, "largestraight":13,
     "twopairs":14, "fullhouse":15
-})
+});
 
 
 function valid_categories() {
     return Object.keys(YatzyCategoriesEnum)
 }
 
+function dice_frequencies(dice) {
+    let answer = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0};
+    dice.forEach(number => answer[number]++);
+    return answer
+}
 
-module.exports = valid_categories
+
+module.exports = {
+    valid_categories: valid_categories,
+    dice_frequencies: dice_frequencies
+};
--- javascript/test/yatzy_vendor1.test.js
+++ javascript/test/yatzy_vendor1.test.js
@@ -1,8 +1,13 @@
 
-const valid_categories = require('../src/yatzy_vendor1');
+const yatzy1 = require('../src/yatzy_vendor1');
 
 
 test('all categories', () => {
-    const categories = valid_categories();
+    const categories = yatzy1.valid_categories();
     expect(categories).toMatchSnapshot();
+});
+
+test('dice frequencies', () => {
+   expect(yatzy1.dice_frequencies([1,2,2,2,2])).toEqual({1:1, 2:4, 3:0, 4:0, 5:0, 6:0});
+   expect(yatzy1.dice_frequencies([6,3,5,2,1])).toEqual({1:1, 2:1, 3:1, 4:0, 5:1, 6:1});
 });
