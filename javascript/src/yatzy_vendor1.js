92802830340c41791cc7598d70f911c55c34bee0
Emily Bache <emily.bache@praqma.com>
2019-11-08 10:27:34 +0100

    yatzy function

--- javascript/src/yatzy_vendor1.js
+++ javascript/src/yatzy_vendor1.js
@@ -1,25 +1,45 @@
 
-const YatzyCategoriesEnum = Object.freeze({
-    "chance":1, "yatzy":2,
-    "ones":3, "twos":4, "threes":5, "fours":6, "fives":7, "sixes":8,
-    "pair":9, "threeofakind":10, "fourofakind":11,
-    "smallstraight":12, "largestraight":13,
-    "twopairs":14, "fullhouse":15
-});
-
-
-function valid_categories() {
-    return Object.keys(YatzyCategoriesEnum)
-}
-
 function dice_frequencies(dice) {
     let answer = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0};
     dice.forEach(number => answer[number]++);
     return answer
 }
 
+function score(dice, category) {
+    return scoring_functions[category](dice);
+}
+
+scoring_functions = Object.freeze({
+    "chance": function chance(dice) {
+        sum = 0;
+        dice.forEach(number => sum += number);
+        return sum;
+    },
+
+    "yatzy": function yatzy(dice) {
+        let frequencies = Object.values(dice_frequencies(dice));
+        if (frequencies.find((n) => n===5))
+            return 50;
+        else
+            return 0;
+    },
+    "ones": function ones(dice) {
+        return 1;
+    },
+
+    "twos":4, "threes":5, "fours":6, "fives":7, "sixes":8,
+    "pair":9, "threeofakind":10, "fourofakind":11,
+    "smallstraight":12, "largestraight":13,
+    "twopairs":14, "fullhouse":15
+});
+
+function valid_categories() {
+    return Object.keys(scoring_functions)
+}
+
 
 module.exports = {
     valid_categories: valid_categories,
-    dice_frequencies: dice_frequencies
+    dice_frequencies: dice_frequencies,
+    score: score
 };
--- javascript/test/yatzy_vendor1.test.js
+++ javascript/test/yatzy_vendor1.test.js
@@ -11,3 +11,16 @@ test('dice frequencies', () => {
    expect(yatzy1.dice_frequencies([1,2,2,2,2])).toEqual({1:1, 2:4, 3:0, 4:0, 5:0, 6:0});
    expect(yatzy1.dice_frequencies([6,3,5,2,1])).toEqual({1:1, 2:1, 3:1, 4:0, 5:1, 6:1});
 });
+
+test('score chance', () => {
+   expect(yatzy1.score([1,2,2,2,2], "chance")).toBe(9);
+});
+
+test('score yatzy', () => {
+    expect(yatzy1.score([1,2,2,2,2], "yatzy")).toBe(0);
+    expect(yatzy1.score([2,2,2,2,2], "yatzy")).toBe(50);
+});
+
+test('score ones', () => {
+    expect(yatzy1.score([1,2,2,2,2], "ones")).toBe(1);
+});
